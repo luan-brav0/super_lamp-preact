c44ea109ebd5c7a04e954d410658298110a5b026
luan-brav0 <luan.melo.bravo10@gmail.com>
2024-09-28 18:16:23 -0300

    started writing websocket | 'isOn' renamed to power

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'preact/hooks'
-import './output.css'
+import { useState, useEffect } from 'preact/hooks';
+import './output.css';
 
 enum Color {
     off,
     red,
     green,
     blue,
-}
+};
 
 type RGB = [number, number, number];
 const colors: RGB[] = [
@@ -16,33 +16,66 @@ const colors: RGB[] = [
     [0, 0, 255]
 ];
 
-/*
-const Colors = () => {
-    return (
-    );
-}
-*/
-
 export function App() {
-    const [isOn, setIsOn] = useState<boolean>(true);
     const [color, setColor] = useState<RGB>(colors[Color.red]);
     const [lastColor, setLastColor] = useState<RGB>(color);
+    const [power, setPower] = useState<boolean>(true);
+
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8080');
+        ws.onopen = () => console.log('WebSocket Connected')
+
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            if (data.type === 'color')
+                setColor(data.color);
+            if (data.type === 'power')
+                setPower(data.power);
+        };
+
+        ws.onerror = (error) => {
+            console.error("WebSocket ERROR:", error);
+        }
+
+        setSocket(ws);
+
+        return () => { ws.close(); };
+    }, []);
+
+    function sendColor(c: RGB): void {
+        if (socket && socket.readyState === WebSocket.OPEN) {
+            const message = JSON.stringify({ type: 'color', color: c });
+            socket.send(message);
+        }
+    }
+
+    function sendPower(power: boolean): void {
+        if (socket && socket.readyState === WebSocket.OPEN) {
+            const message = JSON.stringify({ type: 'power', color: power });
+            socket.send(message);
+        }
+    }
 
     function updateColor(c: RGB): void {
-        if (c == colors[Color.off])
-            setIsOn(false);
+        const isShuttingDown: boolean = c === colors[Color.off];
+        if (isShuttingDown)
+            setPower(false);
         else
-            setIsOn(true);
+            setPower(true);
         // if last color was "off" don't overwrite lastColor
         if (color != colors[Color.off])
             setLastColor(color);
         setColor(c);
+        sendColor(c);
+        sendPower(isShuttingDown);
         return;
     }
 
     function toggle(): void {
         // if it was on, switch it off
-        if (isOn)
+        if (power)
             updateColor(colors[Color.off]);
         else
             updateColor(lastColor ? lastColor : colors[Color.red]);
@@ -66,9 +99,9 @@ export function App() {
                     />
                 )}
             </ol>
-            <button class={isOn ? "on" : "off"}
+            <button class={power ? "on" : "off"}
                 onClick={() => toggle()}>
-                {isOn ? "ON" : "OFF"}
+                {power ? "ON" : "OFF"}
             </button>
         </div>
     );
